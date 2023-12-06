6511c8f0d9c942f200c2780962c0fa289145d90a
Nora Velde <noravelde@gmail.com>
2023-12-06 19:25:44 +0100

    started counting-system

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -3,21 +3,22 @@ import { useLocation, Link } from "react-router-dom"
 import { useState } from "react"
 
 const TestPage = () => {
-
+    const [points, setPoints] = useState(null)
     const location = useLocation()
     console.log(location.state)
 
-    const countingPoints = (point) => {
-        let points = 0;
-        
+    const countingPoints = () => {
+        let count = points + 1;
+        setPoints(count)
     }
 
 return (
     <div className="flagPage">
     <h1>Test your knowledge!</h1>
     <Link to={"/"}>Home</Link>
+    <h3>{points}</h3>
     <div className="chooseCountryContainer">
-      <ChooseCountry continent={location.state} fromPage={"test"}/>  
+      <ChooseCountry continent={location.state} fromPage={"test"} />  
     </div>  
     </div>
 )
